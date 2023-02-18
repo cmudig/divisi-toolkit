ff98944121c9c9d2826b0a6cc2c05709e57519f0
Venkatesh Sivaraman <venkatesh.sivaraman.98@gmail.com>
2023-02-18 16:41:10 -0500

    add tailwind css

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg';
   import { syncValue } from './stores';
 
   export let model;
@@ -8,29 +7,15 @@
 </script>
 
 <main>
-  <div>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Basic Widget Component</h1>
+  <h1 class="text-3xl text-blue-900 font-bold">Basic Widget Component</h1>
 
-  <div class="card">
+  <div class="card m-6 shadow-lg bg-blue-200 rounded">
     The count is {$count}, yay!
   </div>
 </main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
+<style global lang="postcss">
+  @tailwind utilities;
+  @tailwind components;
+  @tailwind base;
 </style>
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import anywidget from 'anywidget/vite';
+import postcss from './postcss.config.js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -34,4 +35,5 @@ export default defineConfig({
       },
     },
   ],
+  css: { postcss },
 });
